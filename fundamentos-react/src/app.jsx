2d0e64487d9e3a0c9e34dd884ab38f4c4bfbab42
demershow <demersontorres520@gmail.com>
2022-08-30 10:30:37 -0300

    Aula 23: adding flex box on card component'

--- fundamentos-react/src/app.jsx
+++ fundamentos-react/src/app.jsx
@@ -1,32 +1,37 @@
 import React from "react";
-
+import "./App.css"
 import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Primeiro from "./components/basicos/Primeiro";
 export default () => (
-  <div>
+  <div className="App">
     <strong>
       <h1>Fundamentos React</h1>
     </strong>
-    <Card titulo="Exercicio #04 - Desafio Aleatório">
-      <Aleatorio min={1} max={100}></Aleatorio>
-    </Card>
 
-    <Card titulo= "Exercicio #03 - fragmento">
+    <div className="Cards">
+      <Card titulo="Exercicio #04 - Desafio Aleatório">
+        <Aleatorio min={1} max={100}></Aleatorio>
+      </Card>
+
+      <Card titulo="Exercicio #03 - fragmento">
         <Fragmento></Fragmento>
-    </Card>
-    <Card titulo="Exercicio #02 - Com Parâmetro">
-      <ComParametro
-      titulo= 'situação do aluno'
-      NomeDoAluno = 'Julio'
-      nota={2.4}
-      ></ComParametro>
-    </Card>
-    <Card titulo="Exercicio #01 - Primeiro component">
-     <Primeiro></Primeiro>
-    </Card>
-   
+      </Card>
+
+      <Card titulo="Exercicio #02 - Com Parâmetro">
+        <ComParametro
+          titulo="situação do aluno"
+          NomeDoAluno="Julio"
+          nota={2.4}
+        ></ComParametro>
+      </Card>
+
+      <Card titulo="Exercicio #01 - Primeiro component">
+        <Primeiro></Primeiro>
+      </Card>
+
+    </div>
   </div>
 );
