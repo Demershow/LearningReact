01cdf8f532a5d250d5ea15ddea4bfed5451c41c8
demershow <demersontorres520@gmail.com>
2022-08-31 11:26:29 -0300

    aula35: state component

--- fundamentos-react/src/components/Comunicação/IndiretaFilho.jsx
+++ fundamentos-react/src/components/Comunicação/IndiretaFilho.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 
 export default props =>{
+    
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50
+    const gerarNerd = () => Math.random() >0.5 
 
     return (
         <div>
             <div>Filho</div>
             <button onClick={
                 function (e) {
-                    props.quandoClicark('joão', 45, true)
+                    props.quandoClicark('joão', gerarIdade(), gerarNerd())
                 }
             }>Fornecer infos</button>
         </div>
--- fundamentos-react/src/components/Comunicação/IndiretaPai.jsx
+++ fundamentos-react/src/components/Comunicação/IndiretaPai.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default props =>{
 
-    let nome ="?"
-    let idade = 0
-    let nerd = false
+    const [nome, setNome] = useState("?")
+    const [idade, setIdade] = useState(0)
+    const [nerd, setNerd] = useState (false)
 
-    function fornecerInfos(nomeParam, idadeParam, nerdParam) {
-        nome = nomeParam
-        idade = idadeParam
-        nerd = nerdParam
-        console.log(nomeParam, idadeParam, nerdParam)
+    function fornecerInfos(nome, idade, nerd) {
+        setNome(nome)
+        setIdade(idade)
+        setNerd(nerd)
+        console.log(nome, idade, nerd)
     }
 
     return (
