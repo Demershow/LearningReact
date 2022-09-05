f60ae1016c7ee66864a91e7d30e7eceaaacf49d5
demershow <demersontorres520@gmail.com>
2022-09-05 09:32:21 -0300

    aula 52: project structure

---
+++ navigation/src/views/examples/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+ 
+
+const About = props => (
+    <div className="About">
+        <h1>About</h1>
+        <p>o nosso aidnasjidniuasdnua</p>
+
+    </div>
+)
+
+export default About;
