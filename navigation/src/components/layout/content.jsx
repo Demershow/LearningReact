89f6ab06a4a172cae951c2498709cd83b05c9ec9
demershow <demersontorres520@gmail.com>
2022-09-05 10:11:31 -0300

    aula 59: 404 page and finish the project

--- navigation/src/components/layout/content.jsx
+++ navigation/src/components/layout/content.jsx
@@ -5,6 +5,7 @@ import About from "../../views/examples/about";
 
 import { Routes, Route } from "react-router-dom";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = (props) => (
   <main className="Content">
@@ -12,6 +13,7 @@ const Content = (props) => (
       <Route path="/" exact element={<Home />} />
       <Route path="/param/:id" exact element={<Param />} />
       <Route path="/about" exact element={<About />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </main>
 );
---
+++ navigation/src/views/examples/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+ 
+
+const NotFound = props => (
+    <div className="NotFound">
+        <h1>404</h1>
+        <h2>Não encontramos a pagina que procuras...</h2>
+
+    </div>
+)
+
+export default NotFound;
