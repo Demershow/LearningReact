846b034c533d9f41893e83906d8e600c8aeddfc9
demershow <demersontorres520@gmail.com>
2022-09-08 08:21:39 -0300

    Aula 75: useReducer #01

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initalState = {
+    number: 0,
+    other: '...',
+    user: null
+}
+
+function reducer (state, action) {
+
+    switch(action.type) {
+        case 'add2':
+            return{...state, number:state.number + 2}
+        case 'login':
+            return {...state, user: {name: action.name}}
+        default:
+            return state
+    }
+
+}
+
+
 const UseReducer = (props) => {
+
+    const [state, exec] = useReducer(reducer, initalState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+            <div className="center">
+                {state.user ? 
+                <span className='text'>{state.user.name}</span> :
+                <span className="text">Usuario nao encontrado!</span>
+                
+            }
+                <span className="text">{state.number}</span>
+                <div>
+                    <button className="btn" onClick={() => exec({type: 'login', name:'Sophia'})}>Login</button>
+                    <button className="btn" onClick={() => exec({type: 'add2'})}>+2</button>
+                </div>
+            </div>
         </div>
     )
 }
