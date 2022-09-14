995c16007dd524fefdde17bfed88b3a412502732
demershow <demersontorres520@gmail.com>
2022-09-14 10:04:36 -0300

    Aula 147: made the onChange Event

--- todoApp/frontend/src/todo/todo.jsx
+++ todoApp/frontend/src/todo/todo.jsx
@@ -4,12 +4,30 @@ import TodoForm from "./todoForm";
 import TodoList from "./todoList";
 
 export default class Todo extends Component {
+
+    constructor(props){
+        super(props)
+        this.state ={description: '', list: []}
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
     render(){
         return(
             <div>
                 <h1>
                     <PageHeader name='Tarefas' small="Cadastro"></PageHeader>
-                    <TodoForm></TodoForm>
+                    <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}></TodoForm>
                     <TodoList></TodoList>
                 </h1>
             </div>
