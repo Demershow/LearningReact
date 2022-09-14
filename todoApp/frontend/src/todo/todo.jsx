215b4afdbbcb9778de77c28a9068706d8e860dfd
demershow <demersontorres520@gmail.com>
2022-09-14 09:24:11 -0300

    143: project structure

--- todoApp/frontend/src/todo/todo.jsx
+++ todoApp/frontend/src/todo/todo.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "React";
 import PageHeader from "../template/page-header";
+import TodoForm from "./todoForm";
+import TodoList from "./todoList";
 
 export default class Todo extends Component {
     render(){
@@ -7,6 +9,8 @@ export default class Todo extends Component {
             <div>
                 <h1>
                     <PageHeader name='Tarefas' small="Cadastro"></PageHeader>
+                    <TodoForm></TodoForm>
+                    <TodoList></TodoList>
                 </h1>
             </div>
         )
