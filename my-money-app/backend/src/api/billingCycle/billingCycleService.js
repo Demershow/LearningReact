b2218d3730efcc9475ac6ab74a009bfdf4a9ee5e
demershow <demersontorres520@gmail.com>
2022-09-20 08:43:39 -0300

    Aula 200: getting the summary of payment

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,3 +1,4 @@
+const billingCycle = require('./billingCycle');
 const BillingCycle = require('./billingCycle');
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
@@ -22,6 +23,22 @@ BillingCycle.route('count', (req, res, next) => {
         }
     })
 });
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{ 
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
+    }, { 
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+    }, { 
+        $project: {_id: 0, credit: 1, debt: 1}
+    }], (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
+        }
+    })
+})
 
 
 module.exports = BillingCycle;
