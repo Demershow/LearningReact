1cc522ba369442f6ee146c2db3ac1db2540afad2
demershow <demersontorres520@gmail.com>
2022-09-21 10:06:03 -0300

    Aula 229: Trying to fix the component

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/TabsHeader'
+import TabContent from '../common/tab/TabContent'
+import TabHeader from '../common/tab/TabHeader'
 
-export default props => (
-    <div className="">
-        <h1>
-            Ciclo pagamento
-        </h1>
-    </div>
-);
+class BillingCycle extends Component {
+    render(){
+        return(
+            <div>
+                <ContentHeader titlte='Ciclos de pagamentos' small='cadastro' />
+                <Content>
+                    <Tabs> 
+                        <TabsHeader>
+                            <TabHeader label='listar' icon='bars' target='tabList'></TabHeader>
+                            <TabHeader label='incluir' icon='plus' target='tabCreate'></TabHeader>
+                            <TabHeader label='alterar' icon='pencil' target='tabUpdate'></TabHeader>
+                            <TabHeader label='excluir' icon='trash-o' target='tabDelete'></TabHeader>
+                        </TabsHeader>
+                        <TabContent>
+
+                        </TabContent>
+                   </Tabs>
+                </Content>
+                
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
