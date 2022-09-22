2b7bd52003157ad05843a983906130b3e8c62e20
demershow <demersontorres520@gmail.com>
2022-09-22 09:23:04 -0300

    Aula 238: Creating the GetList Reducer

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers} from "redux";
 import dashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from "../common/tab/TabReducer";
+import BillingCycleReducer from "../billingCycle/BillingCycleReducer";
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 
