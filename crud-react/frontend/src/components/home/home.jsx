cc7c372c3fa52d406bcabcee8217cf65cfcd0f04
demershow <demersontorres520@gmail.com>
2022-09-29 11:05:43 -0300

    Aula 344: posting on db.json

---
+++ crud-react/frontend/src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/main";
+
+export default (props) => (
+  <Main icon="home" title="incio" subtitle="prj crud">
+    <div className="display-4">Welcome</div>
+    <hr />
+    <div className="mb-0">Explicando a construção de um crud em react</div>
+  </Main>
+);
---
+++ crud-react/frontend/src/components/template/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css'
+import React from 'react'
+
+export default props => (
+    <footer className="footer">
+        <span>
+            Developed with <i className="fa fa-heart text-danger"></i> by
+            <strong> <a href="" target="_blank">Demerson Guilherme Torres</a></strong>
+        </span>
+    </footer>
+)
